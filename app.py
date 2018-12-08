07668055be215fde4237c97e5007b1c538e3a4c0
Brokko-Thinkpad <lennart.re@googlemail.com>
2018-12-08 18:58:30 +0100

    Prüfung der Fragen hinzugefügt

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-import sqlite3 # Import sql
+import sqlite3  # Import sql
 from flask import Flask, render_template, request
 
-app = Flask(__name__) # create app object
+app = Flask(__name__)  # create app object
 
 # Database: Read Data
-connection = sqlite3.connect("questions.db") # Connect to db
-crsr = connection.cursor() # Create cursor object
-myResult = crsr.execute("SELECT * FROM quest;") # Execute a statement
+connection = sqlite3.connect("questions.db")  # Connect to db
+crsr = connection.cursor()  # Create cursor object
+myResult = crsr.execute("SELECT * FROM quest;")  # Execute a statement
 
 # Generieren der Frage-Liste
 items = []
 for row in myResult:
-	items.append({'Index': row[0], 'q': row[1], 'a': row[2], 'b': row[3], 'c': row[4], 'd': row[5], 'ans': row[6], 'id': row[7]})
+    items.append(
+        {'Index': row[0], 'q': row[1], 'a': row[2], 'b': row[3], 'c': row[4], 'd': row[5], 'ans': row[6], 'id': row[7]})
 
 
-# Request-Handler --> Alle Anfragen gehen derzeit über die Root 
+# Request-Handler --> Alle Anfragen gehen derzeit über die Root
 @app.route("/")
 def index():
-    currentQuestion = request.args.get('cq', 0, type=int)
-    # Es wird noch nicht geprüft ob die gegebene Antwort korrekt ist :)
+    # get arguments
+    cq = request.args.get('cq', 0, type=int)  # current question
+    pq = request.args.get('pq', -1, type=int)  # previous question
+    pq_answer = request.args.get('answer', None, type=str)  # previous question
 
-    if currentQuestion >= 0 and currentQuestion < len(items):
-        nextQuestion = -1 if currentQuestion + 1 >= len(items) else currentQuestion + 1
-        return render_template('index.html', myVariable = items[currentQuestion], nq = nextQuestion)
-    if currentQuestion == -1:
-        return "feddisch"
+    # check if answer to previous question was correct
+    pq_correct = -1
+    if 0 <= pq < len(items):
+        pq_correct = 1 if pq_answer == items[pq]['ans'] else 0
+
+    # check if the currently requested question does exist
+    if 0 <= cq < len(items):
+        nextQuestion = -1 if cq + 1 >= len(items) else cq + 1
+        return render_template('index.html', cq=cq, myVariable=items[cq], nq=nextQuestion, pq_correct=pq_correct, pq = pq)
+    elif cq == -1:
+        return render_template('index.html', cq=cq, myVariable=None, nq=None, pq_correct=pq_correct, pq=pq)
     else:
         return "Question doesn't exist"
 
@@ -32,13 +41,11 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
 # To save changes
 connection.commit()
 
 # Close the connection
 connection.close()
-
 
 # Referenzen
 ## http://flask.pocoo.org/docs/1.0/quickstart/#variable-rules
