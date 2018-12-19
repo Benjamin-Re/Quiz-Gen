c78a42f4a8ca87abcead151a5196d3c8c20f600a
Benjamin-Re <renker.benjamin@gmail.com>
2018-12-19 15:18:22 +0100

    Punktesystem, mehr Fragen und neue Route hinzugefügt

--- static/script.js
+++ static/script.js
@@ -1,26 +1,7 @@
 $(document).ready(function () {
 
-	document.getElementById("a").innerHTML = "CHANGE";
-	document.getElementById("quest").innerHTML = "Hello World!";
-	function myFunction() {
-	   document.getElementById("b").innerHTML = "Paragraph changed.";
-	}
 
-	myFunction();
-
-	function test(){
-		document.getElementById("quest").innerHTML = "q changed";
-		document.getElementById("a").innerHTML = "a changed";
-		document.getElementById("b").innerHTML = "b changed";
-		document.getElementById("c").innerHTML = "c changed";
-		document.getElementById("d").innerHTML = "d changed";
-	};
-
-	document.getElementById("next").addEventListener("click", test);
-
-	$.getJSON("testJson.js", function(json) {
-			console.log("JSON Data: " + json[1]);
-		});
-	
+	var demo = $('demo').value;
+	console.log(demo);
 	
 });
